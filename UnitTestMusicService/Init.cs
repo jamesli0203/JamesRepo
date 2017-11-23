10420dc4b56b4a863660b8438b3f3808213085ce
James Li <james_li@outlook.com>
2017-11-23 17:46:38 -0500

    Add test init

---
+++ UnitTestMusicService/Init.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Music.Service;
+using Music.Service.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace UnitTestMusicService
+{
+    [TestClass]
+    public class Init
+    {
+        public static HttpClient TestHttpClient;
+        public static Mock<ArtistRepository> MockArtistRepository;
+
+        [AssemblyInitialize]
+        public static void InitializeTestServer(TestContext testContext)
+        {
+            var testServer = new TestServer(new WebHostBuilder()
+               .UseStartup<Startup>()
+              
+               .UseEnvironment("IntegrationTest"));
+
+            TestHttpClient = testServer.CreateClient();
+        }
+
+        public static void RegisterMockRepositories(IServiceCollection services)
+        {
+            MockArtistRepository = (new Mock<ArtistRepository>());
+            services.AddSingleton(MockArtistRepository.Object);
+        }
+    }
+}
