50f1f1a42540e69f664d453ff3562f160e2b4914
James Li <james_li@outlook.com>
2017-11-23 18:00:04 -0500

    added

--- UnitTestMusicService/UnitTest1.cs
+++ UnitTestMusicService/UnitTest1.cs
@@ -1,4 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Music.Service.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace UnitTestMusicService
 {
@@ -8,6 +13,33 @@ namespace UnitTestMusicService
         [TestMethod]
         public void TestAytistRepository()
         {
+            var mockA = new List<Artist>();
+            mockA.Add(new Artist
+            {
+                Id = 1,
+                Created = DateTime.Today,
+                LastModified = DateTime.Today,
+                Name = "Muse"
+            });
+            mockA.Add(new Artist
+            {
+                Id = 1,
+                Created = DateTime.Today,
+                LastModified = DateTime.Today,
+                Name = "Muse"
+            });
+
+            var employeeRepositoryMock = Init.MockArtistRepository;
+            employeeRepositoryMock.Setup
+                  (x => x.GetAll()).Returns(Task.FromResult(mockA));
+
+            var response = Init.TestHttpClient.GetAsync("api/Employees").Result;
+
+            var resp = response.Content.ReadAsStringAsync().Result;
+            var responseData = JsonConvert.DeserializeObject<List<Artist>>(resp);
+            Assert.AreEqual(3, responseData.Count);
+            Assert.AreEqual(mockA[0].Id, responseData[0].Id);
+
         }
 
         [TestMethod]
